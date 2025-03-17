e938d78307fa670d27470bb1740b076d495d047a
Christopher Attar <cattar@protonmail.ch>
2025-03-16 22:22:30 -0400

    Update TurtleAgent.cs

--- TurtleAgent.cs
+++ TurtleAgent.cs
@@ -1,16 +1,30 @@
 using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgent.Actuators; //Classes related to agent actions
+using Unity.MLAgets.Sensors; // Collect info form environment
 
-public class TurtleAgent : MonoBehaviour
+public class TurtleAgent : Agent
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializedField] private Transform _goal;
+    [SerializedField] private float _moveSpeed = 1.5f;
+    [SerializedField] private float _rotationSpeed =180f;
+    public override void Initialize()
     {
-        
+        Debug.Log("Initialize()");
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void OnEpisodeBegin()
     {
-        
+        Debug.Log("OnEpisodeBegin()");
+    }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        //Placeholder: OBSERVATION DATA
+    }
+
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        //Placeholder: MOVEMENT LOGIC
     }
 }
