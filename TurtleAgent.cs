16d9b87080b98555550f81b2e6b4d50976aee0e1
Christopher Attar <cattar@protonmail.ch>
2025-04-13 12:34:37 -0400

    Update TurtleAgent.cs

--- TurtleAgent.cs
+++ TurtleAgent.cs
@@ -1,44 +1,46 @@
 using UnityEngine;
 using Unity.MLAgents;
-using Unity.MLAgent.Actuators; //Classes related to agent actions
-using Unity.MLAgets.Sensors; // Collect info form environment
+using Unity.MLAgents.Actuators; //Classes related to agent actions
+using Unity.MLAgents.Sensors;
+using UnityEngine.InputSystem; // Collect info form environment
 
 
 //NOTE: WE ARE USING LOCAL BECAUSE THE GAME LIVES INSIDE THE GAME OBJECT OF THE ENVIRONMENT
 //WHEN WE COPY GAME OBJECT TO RUN DIFFERENT INSTANCE OF THE TRAINING, WE DON'T WANT IT TO BE GLOBAL AS IT WILL FAIL
 public class TurtleAgent : Agent
 {
-    [SerializedField] private Transform _goal;
-    [SerializedField] private float _moveSpeed = 1.5f;
-    [SerializedField] private float _rotationSpeed =180f;
+    [SerializeField] private Transform _goal;
+    [SerializeField] private float _moveSpeed = 1.5f;
+    [SerializeField] private float _rotationSpeed =180f;
 
-    private Renderer _renderer;
+    private Renderer _renderer; 
 
-    private int _currentEpisode = 9;
-    private float _cumulativeReward = 0f;
+    [HideInInspector] public int CurrentEpisode = 0;
+    [HideInInspector] public float CumulativeReward = 0f;
     public override void Initialize()
     {
         Debug.Log("Initialize()");
+        _renderer = GetComponent<Renderer>();  // Add this line
     }
 
     public override void OnEpisodeBegin()
     {
         Debug.Log("OnEpisodeBegin()");
-        _currentEpisode++;
-        _cumulativeReward = 0f;
-        _render.material.color = Color.blue;
+        CurrentEpisode++;
+        CumulativeReward = 0f;
+        _renderer.material.color = Color.blue;
 
         SpawnObjects();
     }
 
-    private void Spawnobjects()
+    private void SpawnObjects()
     {
         transform.localRotation= Quaternion.identity;
-        transfor.localPosition = new Vector3(0f, 0.3f, 0f);
+        transform.localPosition = new Vector3(0f, 0.15f, 0f);
 
         //Randomize Direction on the yaxis (angle in deg)
         float randomAngle = Random.Range(0f, 360f);
-        Vector3 randomDirection  Quaternion.Euler(0f, randomAngle, 0f) * Vector3.forward;
+        Vector3 randomDirection = Quaternion.Euler(0f, randomAngle, 0f) * Vector3.forward;
 
         //Randomize the distance with the range [1,2.5]
         float randomDistance = Random.Range(1f, 2.5f);
@@ -53,15 +55,15 @@ public class TurtleAgent : Agent
     public override void CollectObservations(VectorSensor sensor)
     {
         //Goal's Position
-        float goalPosX_normalized = _goal.locationPosition.x / 5f;
-        float goalPosZ_normalized = _goal.locationPosition.z / 5f;
+        float goalPosX_normalized = _goal.localPosition.x / 5f;
+        float goalPosZ_normalized = _goal.localPosition.z / 5f;
 
         //Turtle Pos
         float turtlePosX_normalized = transform.localPosition.x / 5f;
         float turtlePosZ_normalized = transform.localPosition.z /5f;
 
         //Turtle Direction on Y Axis
-        float turtleRotation_normalized = (transform.localRotation.eulerAngles.y / 360f) * 2f - 1.5f
+        float turtleRotation_normalized = (transform.localRotation.eulerAngles.y / 360f) * 2f - 1.5f;
         sensor.AddObservation(goalPosX_normalized);
         sensor.AddObservation(goalPosZ_normalized);
         sensor.AddObservation(turtlePosX_normalized);
@@ -69,9 +71,104 @@ public class TurtleAgent : Agent
         sensor.AddObservation(turtleRotation_normalized);
 
     }
-    
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        var discreteActionsOut = actionsOut.DiscreteActions;
+        discreteActionsOut[0] =  0; //DONT MOVE - DO NOTHING
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            discreteActionsOut[0] = 1;
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            discreteActionsOut[0] = 2;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            discreteActionsOut[0] =3;
+        }
+    }
     public override void OnActionReceived(ActionBuffers actions)
     {
-        //Placeholder: MOVEMENT LOGIC
+        //PMOVE AGENT USING ACTION
+        MoveAgent(actions.DiscreteActions);
+
+        //PENALTY GIVEN EACH STEP. ENCOURAGE QUICK FINISH
+        AddReward(-2f / MaxStep);
+
+        //UPDATE CUMULATIVE REWARD AFTER INCLUD STEP PENALTY
+        CumulativeReward = GetCumulativeReward();
+    }
+
+    public void MoveAgent(ActionSegment<int> act)
+    {
+        var action = act[0];
+
+        switch (action)
+        {
+            case 1: //FORWARD
+                transform.position += transform.forward * _moveSpeed * Time.deltaTime;
+                break;
+            case 2: //ROTATE LEFT
+                transform.Rotate(0f, -_rotationSpeed * Time.deltaTime, 0f);
+                break;
+            case 3: //ROTATE RIGHT
+                transform.Rotate(0f, _rotationSpeed * Time.deltaTime, 0f);
+                break;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Goal"))
+        {
+            GoalReached();
+        }
+    }
+
+    private void GoalReached()
+    {
+        AddReward(1.0f); // LARGE REWARD FOR GOAL
+       CumulativeReward = GetCumulativeReward();
+
+        EndEpisode();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            //Applly small negative reward when collision begins
+            AddReward(-0.05f);
+            if (_renderer != null)
+            {
+                _renderer.material.color = Color.red;
+            }
+        } 
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+        { 
+            // Continually Penalize agent while in contact with wall
+            AddReward(-0.01f * Time.fixedDeltaTime);
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            //Reset wall color when colligion ends
+            if(_renderer != null)
+            { 
+            // Continually Penalize agent while in contact with wall
+            _renderer.material.color = Color.blue;
+            }
+        }
     }
 }
+
+
